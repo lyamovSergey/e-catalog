b775a465b571fe0dae49c4e405ec4811c576eee3
Lyamov Sergey <lyamofff@gmail.com>
2020-05-11 17:55:54 +0300

    add pages

--- e-catalog main/app/js/main.js
+++ e-catalog main/app/js/main.js
@@ -1,3 +1,30 @@
-$(function(){
-   
+$(function() {
+
+
+
+    $('.item').on('click', function() {
+        $(this).toggleClass('zoom')
+    })
+
+    $('.inputs-block input[type="checkbox"]').on('input', function() {
+        let n = $("input:checked").length;
+        if (n != 0) {
+            $('.aside-footer').addClass('checked-filters')
+        } else {
+            $('.aside-footer').removeClass('checked-filters')
+        }
+    });
+
+    $('.closeFilter').on('click', function() {
+        $('aside').animate({ right: '-230px' }, 500)
+    })
+    $('.filter-button').on('click', function() {
+        $('aside').animate({ right: '0' }, 500)
+    })
+
+
+
+
+
+
 })
